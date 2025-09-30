adacf23ee5f6a93636cf8b84a8bb2805be0f0b3e
motp10 <mott180606@gmail.com>
2025-09-30 12:47:41 +0300

    delet interfaces for train route simulator

--- src/Lab1/Routes/IRoute.cs
+++
@@ -1,8 +0,0 @@
-using Itmo.ObjectOrientedProgramming.Lab1.Roads;
-
-namespace Itmo.ObjectOrientedProgramming.Lab1.Routes;
-
-public interface IRoute
-{
-    IReadOnlyCollection<ITrackSection> Sections { get; }
-}
--- src/Lab1/Routes/IRouteSimulator.cs
+++
@@ -1,9 +0,0 @@
-using Itmo.ObjectOrientedProgramming.Lab1.ResultTypes;
-using Itmo.ObjectOrientedProgramming.Lab1.Trains;
-
-namespace Itmo.ObjectOrientedProgramming.Lab1.Routes;
-
-public interface IRouteSimulator
-{
-    ResultType Simulate(Route route, ITrain train, decimal time = 1);
-}
